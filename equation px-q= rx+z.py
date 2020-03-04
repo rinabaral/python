b0664f638329903e8f979973c95e80d6415feb23
rinabaral <rina12baral@gmail.com>
2020-03-04 10:55:30 +0545

    ...

--- equation px-q= rx+z.py
+++ equation px-q= rx+z.py
@@ -1,10 +1,13 @@
+import sympy as sym
 print("To solve equation px-q = rx+z ")
 p= int(input("Enter the value of p ="))
 q= int(input("Enter the value of q ="))
 r= int(input("Enter the value of r ="))
 z= int(input("Enter the value of z ="))
-if (p-r) == 0:
-    print("The value is infinite")
-else:
-    x=(q+z)/(p-r)
-    print("The value of x is: ",x)
+
+x = sym.symbols('x')
+eq1 = sym.Eq(p*x-q*x-z-q,x)
+sol = sym.solve(eq1)
+print("The value of x is :",sol)
+
+
